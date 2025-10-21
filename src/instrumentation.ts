e738e113ab385456e095cad6886398e1af68c5d3
Diego Artiles <diego@dartiles.dev>
2025-10-21 12:17:51 -0300

    feat: implement onRequestError function for enhanced error logging during request handling

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,3 +1,4 @@
+import { log } from "@/lib/logger/logger";
 import { OTLPLogExporter } from "@opentelemetry/exporter-logs-otlp-http";
 import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
@@ -37,4 +38,27 @@ export function register() {
       ],
     });
   }
+}
+
+export function onRequestError(
+  error: { digest: string } & Error,
+  request: {
+    path: string; // resource path, e.g. /blog?name=foo
+    method: string; // request method. e.g. GET, POST, etc
+    headers: { [key: string]: string | string[] };
+  },
+  context: {
+    routerKind: "Pages Router" | "App Router"; // the router type
+    routePath: string; // the route file path, e.g. /app/blog/[dynamic]
+    routeType: "render" | "route" | "action" | "middleware"; // the context in which the error occurred
+    renderSource:
+      | "react-server-components"
+      | "react-server-components-payload"
+      | "server-rendering";
+    revalidateReason: "on-demand" | "stale" | undefined; // undefined is a normal request without revalidation
+    renderType: "dynamic" | "dynamic-resume"; // 'dynamic-resume' for PPR
+  },
+) {
+  const errorMessage = `Unhandled error in route "${context.routePath}" [${context.routeType}]: ${error?.message ?? "No error message provided"}`;
+  log.error(errorMessage, { error, request, context }, null);
 }
