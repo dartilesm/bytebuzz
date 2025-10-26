fdd33421611cc8ccd803cf50fc74879c80ea307e
Diego Artiles <diego@dartiles.dev>
2025-10-26 19:53:24 -0300

    refactor: simplify user profile loading component by removing Skeleton placeholders and adjusting layout

--- src/components/user-profile/loading/user-profile-content.loading.tsx
+++ src/components/user-profile/loading/user-profile-content.loading.tsx
@@ -1,16 +1,11 @@
 "use client";
 
 import { UserPostLoading } from "@/components/loading/user-post.loading";
-import { Skeleton } from "@heroui/react";
 
 export function UserProfileContentLoading() {
   return (
     <div className='flex flex-col gap-2'>
-      <div className='flex gap-4 border-b border-default-200'>
-        <Skeleton className='w-16 h-10 rounded-none' />
-        <Skeleton className='w-16 h-10 rounded-none' />
-        <Skeleton className='w-16 h-10 rounded-none' />
-      </div>
+      <div className='flex gap-4 border-b border-default-200 h-10' />
       <div className='py-4 flex flex-col gap-2'>
         <UserPostLoading />
         <UserPostLoading />
