ce0000ec0d443da060634ac4e8312b13b625e41b
Diego Artiles <diego@dartiles.dev>
2025-11-23 21:05:01 -0300

    refactor: Update background gradient colors in UserProfileCoverAvatar for improved visual consistency

--- src/components/user-profile/user-profile-cover-avatar.tsx
+++ src/components/user-profile/user-profile-cover-avatar.tsx
@@ -14,7 +14,7 @@ export function UserProfileCoverAvatar({ profilePromise }: UserProfileCoverAvata
   return (
     <div className="space-y-4">
       <div className="w-full aspect-12/4 relative">
-        <div className="absolute top-0 md:-left-4 -left-2 w-[calc(100%+16px)] md:w-[calc(100%+32px)] h-full bg-linear-to-bl from-blue-100 to-violet-200">
+        <div className="absolute top-0 md:-left-4 -left-2 w-[calc(100%+16px)] md:w-[calc(100%+32px)] h-full bg-linear-to-bl from-gray-200 to-gray-300 dark:from-gray-800 dark:to-gray-900">
           {
             profile.cover_image_url && (
               <Image
