0844b54255632755700572fd3d1af250a84a562d
Diego Artiles <diego@dartiles.dev>
2025-11-25 01:27:19 -0300

    chore: Update VSCode settings to use Prettier as the default formatter for TypeScript React files

--- src/components/containers/explore-view/explore-view-users.tsx
+++ src/components/containers/explore-view/explore-view-users.tsx
@@ -2,8 +2,8 @@
 
 import { ExploreViewEmpty } from "@/components/containers/explore-view/explore-view-empty";
 import { UserVerticalCard } from "@/components/explore/user-vertical-card";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import type { userService } from "@/lib/db/services/user.service";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import type { Tables } from "database.types";
 import { use } from "react";
 
