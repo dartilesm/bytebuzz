4be936b913e5609a8fa6d22b0425466a55e3387b
Diego Artiles <diego@dartiles.dev>
2025-11-30 10:27:03 -0300

    feat: add SignIn page component with OAuth flow support and fallback redirect URL

--- src/app/(social)/@modal/(.)sign-up/[[...sign-up]]/page.tsx
+++ src/app/(social)/@modal/(.)sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,6 @@
 import { SignUp } from "@clerk/nextjs";
 
-export default async function SignUpPage({ searchParams }: PageProps<"/sign-up">) {
+export default async function SignUpPage({ searchParams }: PageProps<"/sign-up/[[...sign-up]]">) {
   const { redirectUrl } = await searchParams;
 
   return <SignUp oauthFlow='popup' fallbackRedirectUrl={(redirectUrl as string) || "/"} />;
