a87cda979be172de2fc631bf0f5920272a87af8c
Diego Artiles <diego@dartiles.dev>
2025-11-30 11:05:32 -0300

    feat: enhance ModalLayout with open state management and navigation handling

--- src/app/(social)/@modal/layout.tsx
+++ src/app/(social)/@modal/layout.tsx
@@ -1,11 +1,26 @@
 "use client";
 
 import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function ModalLayout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const [isOpen, setIsOpen] = useState(true);
+
+  useEffect(() => {
+    setIsOpen(true);
+  }, []);
+
+  function handleOnOpenChange(open: boolean) {
+    if (!open) {
+      setIsOpen(false);
+      router.back();
+    }
+  }
   return (
-    <Dialog defaultOpen>
-      <DialogContent className="max-w-[95vw] md:max-w-xl p-0 overflow-hidden border-none bg-transparent shadow-none [&>button]:hidden">
+    <Dialog open={isOpen} onOpenChange={handleOnOpenChange}>
+      <DialogContent className='max-w-[95vw] md:max-w-xl p-0 overflow-hidden border-none bg-transparent shadow-none [&>button]:hidden'>
         {children}
       </DialogContent>
     </Dialog>
