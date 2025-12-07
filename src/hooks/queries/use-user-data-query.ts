06c169cae6c712e8377201594fcc7e825488529a
Diego Artiles <diego@dartiles.dev>
2025-12-07 13:42:09 -0300

    refactor: remove unused hooks related to user and post management for improved code cleanliness

--- src/hooks/queries/use-user-data-query.ts
+++
@@ -1,6 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { userQueries } from "@/hooks/queries/options/user-queries";
-
-export function useUserDataQuery(userId: string) {
-  return useQuery(userQueries.data(userId));
-}
