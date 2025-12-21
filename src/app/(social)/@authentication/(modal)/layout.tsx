d9767ebd91dc3a325ceacbe64b8e14d4f5be1468
Diego Artiles <diego@dartiles.dev>
2025-12-21 12:41:54 -0300

    refactor: simplify authentication modal layout by removing unused state and console logs

--- src/app/(social)/@authentication/(modal)/layout.tsx
+++ src/app/(social)/@authentication/(modal)/layout.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 
 export default function AuthenticationModalLayout({ children }: { children: React.ReactNode }) {
@@ -10,25 +9,15 @@ export default function AuthenticationModalLayout({ children }: { children: Reac
   const pathname = usePathname();
   const initialPathnameRef = useRef<string>(pathname);
   const isClosingRef = useRef<boolean>(false);
-  const [isOpen, setIsOpen] = useState(true);
 
   useEffect(() => {
-    setIsOpen(true);
-  }, []);
-
-  useEffect(() => {
-    console.log("pathname", pathname);
-    console.log("isClosingRef", isClosingRef.current);
-    console.log("initialPathnameRef", initialPathnameRef.current);
-    console.log("isOpen", isOpen);
-    console.log("children", children);
     // If the pathname changes and isClosingRef is true, means the user's clicked outside the modal or intentionally closed it
     // so we need to close the modal by going back to the previous page until we get out from the parallel route
     if (isClosingRef.current) handleOnOpenChange(false);
   }, [pathname]);
 
   function handleOnOpenChange(open: boolean) {
-    console.log("handleOnOpenChange", open); // If the modal is already open, do nothing
+    // If the modal is already open, do nothing
     if (open) return;
     // If current pathname is different from the initial pathname, set isClosingRef to true
     // to keep going back until we get out from the parallel route
@@ -37,9 +26,8 @@ export default function AuthenticationModalLayout({ children }: { children: Reac
     router.back();
   }
   return (
-    <Dialog open={isOpen} onOpenChange={handleOnOpenChange}>
+    <Dialog defaultOpen onOpenChange={handleOnOpenChange}>
       <DialogContent className="max-w-[95vw] md:max-w-xl p-0 overflow-hidden border-none bg-transparent shadow-none [&>button]:hidden">
-        <Link href="/root">Go back</Link>
         {children}
       </DialogContent>
     </Dialog>
