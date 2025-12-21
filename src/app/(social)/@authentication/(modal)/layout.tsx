89deaac57c0a7eaef0cf17e43741a009df6d7abe
Diego Artiles <diego@dartiles.dev>
2025-12-21 12:40:08 -0300

    fix: add catch-all route to properly close authentication modal on navigation

--- src/app/(social)/@authentication/(modal)/layout.tsx
+++ src/app/(social)/@authentication/(modal)/layout.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { Dialog, DialogContent } from "@/components/ui/dialog";
+import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
 
 export default function AuthenticationModalLayout({ children }: { children: React.ReactNode }) {
   const router = useRouter();
@@ -16,13 +17,18 @@ export default function AuthenticationModalLayout({ children }: { children: Reac
   }, []);
 
   useEffect(() => {
+    console.log("pathname", pathname);
+    console.log("isClosingRef", isClosingRef.current);
+    console.log("initialPathnameRef", initialPathnameRef.current);
+    console.log("isOpen", isOpen);
+    console.log("children", children);
     // If the pathname changes and isClosingRef is true, means the user's clicked outside the modal or intentionally closed it
     // so we need to close the modal by going back to the previous page until we get out from the parallel route
     if (isClosingRef.current) handleOnOpenChange(false);
   }, [pathname]);
 
   function handleOnOpenChange(open: boolean) {
-    // If the modal is already open, do nothing
+    console.log("handleOnOpenChange", open); // If the modal is already open, do nothing
     if (open) return;
     // If current pathname is different from the initial pathname, set isClosingRef to true
     // to keep going back until we get out from the parallel route
@@ -32,7 +38,8 @@ export default function AuthenticationModalLayout({ children }: { children: Reac
   }
   return (
     <Dialog open={isOpen} onOpenChange={handleOnOpenChange}>
-      <DialogContent className='max-w-[95vw] md:max-w-xl p-0 overflow-hidden border-none bg-transparent shadow-none [&>button]:hidden'>
+      <DialogContent className="max-w-[95vw] md:max-w-xl p-0 overflow-hidden border-none bg-transparent shadow-none [&>button]:hidden">
+        <Link href="/root">Go back</Link>
         {children}
       </DialogContent>
     </Dialog>
